4f51b1903ce109d6652fa0f22e56924580581824
p <101421945+sirpae@users.noreply.github.com>
2022-05-17 18:23:59 +0100

    uncompleted - still going.

--- src/data-types/numbers.js
+++ src/data-types/numbers.js
@@ -6,22 +6,23 @@ const numThree = 32
 // TODO: Add code below using Javascript numeric operators so that the tests pass
 
 // Set this variable to numOne added to numTwo
-const numOnePlusNumTwo = NaN
+const numOnePlusNumTwo = numOne + numTwo;
+
 
 // Set this variable to numThree multiplied by numTwo
-const numThreeTimesNumTwo = NaN
+const numThreeTimesNumTwo = numThree * numTwo;
 
 // Set this variable to numThree divided by numOne
-const numThreeDividedByNumOne = NaN
+const numThreeDividedByNumOne = numThree / numOne;
 
 // Set this variable to numThree minus numOne
-const numThreeMinusNumOne = NaN
+const numThreeMinusNumOne = numThree - numOne;
 
 // Set this variable to the sum of numOne, numTwo and numThree
-const sum = NaN
+const sum = numOne + numTwo + numThree;
 
 // Set this variable to the sum of (numOne, numTwo, numThree) divided by numOne
-const numBytes = NaN
+const numBytes = (numOne + numTwo + numThree) / numOne;
 
 // do not edit the exported object.
 module.exports = {
